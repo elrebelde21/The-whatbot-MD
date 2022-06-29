3bdf8e0fea40f1c0e4a2c5c7f666ef4dad851fe1
The whatBots420 <105924903+Subbotsito@users.noreply.github.com>
2022-06-29 10:13:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ global.mods = []
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'creador' , 'creador' , true
+global.nameowner = 'creado' , 'creador' , true
 global.numberowner = '56977774748' , '51935116539' , true
 global.instagram = ''
 global.github = ''
